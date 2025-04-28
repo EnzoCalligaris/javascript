a1653a6f1125a3bb7097a12bd44162cc46e37480
Enzo Carvalho Calligaris <enzocontatobr@gmail.com>
2025-04-28 16:59:19 -0300

    aula 14 finalizada

--- aula14 ex/ex17/script.js
+++ aula14 ex/ex17/script.js
@@ -1,1 +1,17 @@
-alert('Olá!')
+function tabuada(){
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if(num.value.lenght == 0){
+        window.alert('[ERRO] Digite um número válido!')
+    } else{
+        tab.innerHTML = ''
+        var n = Number(num.value)
+        for(var c = 1; c <= 10; c++){
+            var item = document.createElement('option')
+            item.text= `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+    
+}
